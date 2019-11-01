b6b5a814ac187d3466eab6351298fbc6e418bbc6
wangyuning <wangyuning@shuidihuzhu.com>
2019-11-01 16:39:29 +0800

    更改status字段类型

--- docs/assets/interfaces.js
+++ docs/assets/interfaces.js
@@ -2167,7 +2167,7 @@ const interfaces = {
                     desc: ''
                 },
                 {
-                    key: 'Status',
+                    key: 'status',
                     name: '体检状态',
                     type: 'Integer',
                     isReq: 'Y',
@@ -2241,9 +2241,9 @@ const interfaces = {
                     desc: 'yyyy-MM-dd HH:mm:ss'
                 },
                 {
-                    key: 'Status',
+                    key: 'status',
                     name: '状态',
-                    type: 'String',
+                    type: 'Integer',
                     isReq: 'Y',
                     desc: '1:退保;2:失效'
                 }
